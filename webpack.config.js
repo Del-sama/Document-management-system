4fab0d803fc2060b72093a2fa1147bf296a1d017
Delores Diei <delores.diei@andela.com>
2017-03-25 23:44:35 +0100

    chore(configure-webpack): fix eslint violations [#142443959]

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  context: __dirname + '/server/app',
+  context: `${__dirname} /server/app`,
 
   entry: './js/app.js',
 
   output: {
     filename: 'app.js',
-    path: __dirname + '/dist',
+    path: `${__dirname} /dist`,
   }
 };
