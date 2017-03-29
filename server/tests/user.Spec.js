01a2d57fdebcf18b00ea616de586521f4236eadc
Delores Diei <delores.diei@andela.com>
2017-03-29 11:30:58 +0100

    chore(set-up-unit-test): modify test to suit chai expect [142439875]

--- server/tests/user.Spec.js
+++ server/tests/user.Spec.js
@@ -1,10 +1,13 @@
+const chai = require('chai');
+const expect = require('chai').expect;
+
 describe('A suite is just a function', () => {
   let a;
 
   it('and so is a spec', () => {
     a = true;
 
-    expect(a).toBe(true);
+    expect(a).to.equal(true);
   });
 });
 
