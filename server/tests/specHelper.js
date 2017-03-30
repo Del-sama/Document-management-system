6379222cbc1880707b872d434532b923a06d066b
Delores Diei <delores.diei@andela.com>
2017-03-30 19:03:56 +0100

    chore(testing-models): create test helper [142500895]

---
+++ server/tests/specHelper.js
@@ -0,0 +1,66 @@
+const faker = require('faker');
+const bcrypt = require('bcrypt-nodejs');
+
+module.exports = {
+  testRole: {
+    title: 'admin'
+  },
+
+  testRole2: {
+    title: 'regular'
+  },
+
+  testUser: {
+    user_name: faker.internet.userName(),
+    first_name: faker.name.firstName(),
+    last_name: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: 'deloresdiei'
+  },
+
+  testUser2: {
+    userName: faker.internet.userName(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  },
+
+  testUser3: {
+    userName: faker.internet.userName(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  },
+
+  testDocument: {
+    title: faker.lorem.sentence(7),
+    content: faker.lorem.paragraphs()
+  },
+
+  testDocument2: {
+    title: faker.finance.accountName(),
+    content: faker.lorem.paragraph(),
+    access: 'private'
+  },
+
+  testDocument3: {
+    title: faker.commerce.department(),
+    content: faker.lorem.paragraph()
+  },
+
+  documentsCollection() {
+    const documentsParams = [];
+
+    for (let i = 0; i <= 15; i += 1) {
+      documentsParams.push({
+        title: faker.lorem.sentence(7),
+        content: faker.lorem.paragraph(),
+        OwnerId: 1
+      });
+    }
+
+    return documentsParams;
+  }
+};
