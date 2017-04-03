a87774ea96c2cbd31d78eabdf9dff0f0451273e2
Delores Diei <delores.diei@andela.com>
2017-04-03 20:08:32 +0100

    feature(user-signup): add authentication [142894273]

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,40 +1,4 @@
-<<<<<<< HEAD
 const jwt = require('jsonwebtoken');
-const model = require('../models');
-
-const UsersController  {
-  static createUser {
-    const newUser = (request, response) {
-      model.User.findOne({where: { username: request.body.user}})
-    }
-  }
-}
-var newUser = function (req, res){
-  User.findOne({where:{ username: req.body.username }})
-    .then(function (user) {
-      if(!user){
-        User.create({ 
-          username: req.body.username, 
-          password: req.body.password, 
-          email: req.body.email 
-        })
-        .then(function(user){
-              var myToken = jwt.sign({ user: user.id },
-                                      'secret',
-                                     { expiresIn: 24 * 60 * 60 });
-              res.send(200, {'token': myToken,
-                             'userId':    user.id,
-                             'username': user.username });
-        });
-      } else {
-        res.status(404).json('Username already exist!');
-      }
-    })
-    .catch(function (err) {
-      res.send('Error creating user: ', err.message);
-    });
-};
-=======
 const model = require('../models');
 
 class UsersController {
@@ -42,7 +6,17 @@ class UsersController {
    model.User.findOne({ where: { userName: request.body.userName } })
      .then((user) => {
        if (!user) {
-         model.User.create(request.body);
+         model.User.create(request.body)
+           .then((newUser) => {
+             const token = jwt.sign({
+               UserId: newUser.id,
+               RoleId: newUser.RoleId
+             }, 'secret', { expiresIn: '2 days' });
+             return response.status(201)
+             .send({ newUser, token, expiresIn: '2 days' });
+           })
+           .catch(error => response.status(400)
+             .send(error.errors));
        }
        return response.status(409)
          .send({ message: `${request.body.userName} is already in use` });
@@ -51,4 +25,3 @@ class UsersController {
 }
 
 module.exports = UsersController;
->>>>>>> 1931d16cdda461b4dd2534b1f95dd6bdf6a9ca05
