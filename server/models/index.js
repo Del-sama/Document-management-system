4d5eed557a6f2ee55d39f92f7a1892de3e79a2d1
Delores Diei <delores.diei@andela.com>
2017-04-05 08:37:12 +0100

    feature(get-users): refactor code [14289305]

--- server/models/index.js
+++ server/models/index.js
@@ -4,7 +4,8 @@ const Sequelize = require('sequelize');
 
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config.json`)[env];
+const config = require('../config.json')[env];
+
 const db = {};
 
 // if (config.use_env_variable) {
