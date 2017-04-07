1abfb4ba2acb5eac6407ca007e6f6ab4cf9fbc7d
Delores Diei <delores.diei@andela.com>
2017-04-07 03:36:41 +0100

    feature(delete-user): add delete user route[142894449]

--- server/routes/user.js
+++ server/routes/user.js
@@ -12,6 +12,7 @@ router.route('/users')
 
 router.route('/users/:id')
   .get(auth.verifyToken, usersController.getUser)
-  .put(auth.verifyToken, usersController.updateUser);
+  .put(auth.verifyToken, usersController.updateUser)
+  .delete(auth.verifyToken, usersController.deleteUser);
 
 module.exports = () => router;
