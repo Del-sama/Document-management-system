7a8b6249843a4425f593ff52a0e7e4058c00b57a
Delores Diei <delores.diei@andela.com>
2017-04-07 07:33:49 +0100

    feature(create-roles): add roles controller [143297581]

---
+++ server/controllers/role.js
@@ -0,0 +1,24 @@
+const model = require('../models');
+
+/**
+ * Class UsersController
+ * To handle routing logic for documents route
+ */
+class RolesController {
+
+/**
+   * Method getUsers to obtain all users
+   * @param {object} request - request object
+   * @param {object} response - response object
+   * @returns {Object} response object
+   */
+  static createRoles(request, response) {
+    model.Role.Create(request.body)
+      .then(newRole => response.status(201)
+          .send(newRole))
+      .catch(error => response.status(400)
+        .send(error.errors));
+  }
+}
+
+module.exports = RolesController;
