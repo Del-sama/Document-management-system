26e4a18c4e3f3616bf40df0997d4a9df91eb2117
Delores Diei <delores.diei@andela.com>
2017-04-12 03:48:53 +0100

    feature(effective-landingpage): refactor code[143502291]

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,39 @@
+const webpack = require('webpack');
+const path = require('path');
+
 module.exports = {
-  context: `${__dirname}/client/app`,
-
-  entry: './js/app.js',
-
-  output: {
-    filename: 'bundle.js',
-    path: `${__dirname}/dist`,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json']
-  },
+  entry: [
+    './client/src/index'
+  ],
   module: {
     loaders: [
       {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loaders: ['babel-loader']
+        test: /\.js?$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel',
+        exclude: /node_modules/
       }
     ]
-  }
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: path.join(__dirname, 'client/dist/'),
+    publicPath: '/app/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './client/dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
 };
-
