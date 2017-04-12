3f56985c1f1e2c27dcd3c55ef0fe6848cfa33270
Delores Diei <delores.diei@andela.com>
2017-04-12 13:09:27 +0100

    feature(effective-landingpage): refactor code[143502291]

--- client/src/components/nav.component.js
+++ client/src/components/nav.component.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 
-class Nav extends Component {
+class Navbar extends Component {
+  /**
+   * Creates an instance of Navbar.
+   * @param {any} props
+   * @memberOf Navbar
+   */
+  constructor(props) {
+    super(props);
+  }
+  /**
+   * renders the Nav component
+   * @returns {void}
+   * @memberOf Navbar
+   */
   render() {
     return (
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>Login</li>
-          <li>Sign up</li>
-        </ul>
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          <div className="brand-logo"><a href="/app/">Document Management System</a></div>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li className={this.props.isLoginActive}>
+              <a href="#">Login</a>
+            </li>
+            <li className={this.props.isSignupActive}>
+              <a href="#">Signup</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
-export default Nav;
+
+Navbar.defaultProps = {
+  isLoginActive: 'active',
+  isSignupActive: ''
+};
+export default Navbar;
