148b7bf4b9012ffce9c0f54cb4236c96b8aec29d
Delores Diei <delores.diei@andela.com>
2017-04-12 04:34:24 +0100

    feature(effective-landingpage): add file[143502291]

---
+++ client/src/components/body.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Body extends Component {
+  render() {
+    return (
+      <div>
+        <img src="../images/files.jpeg" />
+      </div>
+    );
+  }
+}
+export default Body;
---
+++ client/src/components/nav.component.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>Login</li>
+          <li>Sign up</li>
+        </ul>
+      </div>
+    );
+  }
+}
+export default Nav;
