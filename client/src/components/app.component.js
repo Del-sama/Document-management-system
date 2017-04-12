62d20018f57a1f519346e84a11e68c3ec5026606
Delores Diei <delores.diei@andela.com>
2017-04-12 13:08:34 +0100

    feature(effective-landingpage): add login and sign up routes[143502291]

--- client/src/components/app.component.js
+++ client/src/components/app.component.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router'
-import { Router, Switch, BrowserRouter } from 'react-router-dom'
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import Index from './index.component';
-import Signup from './containers/SignupPage.jsx';
-import Login from './containers/LoginPage.jsx';
 
-class App extends Component{
-    render(){
-        return (
-            <BrowserRouter>
-                <Switch>
-                    {/*<Route exact path='/' component={Login} />*/}
-                    <Route path='/app' component={Index} />
-                    <Route exact path='/' component={Signup} />
-                </Switch>
-            </BrowserRouter>
-        );
-    }
+class App extends Component {
+  /**
+   * renders the app component
+   * @returns {void}
+   * @memberOf App
+   */
+  render() {
+    return (
+      <BrowserRouter>
+        <Route path='/app' component={Index} />
+      </BrowserRouter>
+    );
+  }
 }
+
 export default App;
 
