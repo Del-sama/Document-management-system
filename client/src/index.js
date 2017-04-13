98bc66e759377fa52a2c17d4ce7fe6035f19f222
Delores Diei <delores.diei@andela.com>
2017-04-13 20:43:30 +0100

    feature(login-signup): change file location [143550727]

--- client/src/index.js
+++
@@ -1,5 +0,0 @@
-const ReactDom = require('react-dom');
-const React = require('react');
-import App from './components/app.component'
-
-ReactDom.render(<App />, document.getElementById('react-app'));
