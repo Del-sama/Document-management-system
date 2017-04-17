47cc2e74d48574dd3686807e8b1de69561e0e6c8
Delores Diei <delores.diei@andela.com>
2017-04-17 19:44:35 +0100

    feature(login-signup): crete login action [143550727]

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,23 @@
-const ReactDom = require('react-dom');
-const React = require('react');
-const sass = require('../scss/main.scss');
-import App from './components/app.component'
+import ReactDom from 'react-dom';
+import React from 'react';
+import { render } from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import App from './components/app.component';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+// import routes from '../../../server/routes';
+require('../scss/main.scss');
 
-ReactDom.render(<App />, document.getElementById('react-app'));
+
+const store = createStore(
+  rootReducer,
+  (state = {}) => state,
+  applyMiddleware(thunk)
+);
+
+ReactDom.render(
+<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('react-app'));
