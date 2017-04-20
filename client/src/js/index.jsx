f2f8c0536711e0881b4041a0b930d3b27557d539
Delores Diei <delores.diei@andela.com>
2017-04-20 01:24:08 +0100

    feature(login-signup): change file location [143550727]

---
+++ client/src/js/index.jsx
@@ -0,0 +1,23 @@
+import ReactDom from 'react-dom';
+import React from 'react';
+import { render } from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import App from './components/app.component';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+
+require('../scss/main.scss');
+
+
+const store = createStore(
+  rootReducer,
+  (state = {}) => state,
+  applyMiddleware(thunk)
+);
+
+ReactDom.render(
+<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('react-app'));
