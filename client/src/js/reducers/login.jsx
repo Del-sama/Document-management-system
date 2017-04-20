40551ee95eb92001ca69e4f7b8e02f1003355591
Delores Diei <delores.diei@andela.com>
2017-04-20 01:26:15 +0100

    feature(login-signup): create file [143550727]

---
+++ client/src/js/reducers/login.jsx
@@ -0,0 +1,10 @@
+export default (state = {}, action) => {
+  switch(action.type){
+    case 'LOGIN_SUCCESSFUL':
+      return {...state, user: action.user, token: action.token, error: null, success: action.message}
+    case 'LOGIN_ERROR':
+      return {...state, error: action.message, success: null}
+    default:
+      return state;
+  }
+}
