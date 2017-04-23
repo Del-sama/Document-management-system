db5a3f27d0b4d005460c2773f393ef6d0645db3c
Delores Diei <delores.diei@andela.com>
2017-04-23 10:04:20 +0100

    feature(edit-documents): refactor code [144148203]

--- client/src/js/components/dashboard/Index.component.jsx
+++ client/src/js/components/dashboard/Index.component.jsx
@@ -22,6 +22,21 @@ class Dashboard extends Component {
     const userId = this.state.authUser.UserId || null
     this.props.actions.viewUserDocuments(userId);
   }
+  componentDidMount() {
+    $('.modal').modal({
+      dismissible: false, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      inDuration: 300, // Transition in duration
+      outDuration: 200, // Transition out duration
+      startingTop: '4%', // Starting top style attribute
+      endingTop: '10%', // Ending top style attribute
+      // ready: function (modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+      //   alert("Ready");
+      //   console.log(modal, trigger);
+      // },
+      complete: function () { alert('Closed'); } // Callback for Modal close
+    });
+  }
 
   render() {
     const roleId = this.state.authUser.role_id || null
