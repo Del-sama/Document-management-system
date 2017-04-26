9ebc84513ead9b594e73d63abc011c82c06abeb1
Delores Diei <delores.diei@andela.com>
2017-04-26 15:38:53 +0100

    change action request path

--- client/src/js/actions/authorizationManagement/signupAction.js
+++ client/src/js/actions/authorizationManagement/signupAction.js
@@ -5,7 +5,7 @@ export const loginSuccessful = user => ({ type: 'LOGIN_SUCCESSFUL', user });
 
 export default (userData) => {
   return (dispatch) => {
-    return axios.post('/users', userData)
+    return axios.post('/app/users', userData)
       .then((response) => {
         window.localStorage.setItem('token', response.data.token);
         const user = jwtDecode(response.data.token);
