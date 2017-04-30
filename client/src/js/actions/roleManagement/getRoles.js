0c726b707d254c1feb89ba905a1ed1d8b983d20a
Delores Diei <delores.diei@andela.com>
2017-04-30 16:52:56 +0100

    feature(get-all-users): add file [144499885]

---
+++ client/src/js/actions/roleManagement/getRoles.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { browserHistory } from 'react-router';
+import * as actionTypes from '../actionTypes';
+
+const roleGetSuccess = (roles) => {
+  return {
+    type: actionTypes.GET_ROLE_SUCCESS,
+    roles
+  };
+};
+
+export const viewRoles = (userId) => {
+  return (dispatch) => {
+    const token = window.localStorage.getItem('token');
+    axios.get('/roles', {
+      headers: {
+        Authorization: token
+      }
+    })
+    .then((roles) => {
+      dispatch(roleGetSuccess(roles));
+    })
+    .catch((err) => {
+    });
+  };
+};
