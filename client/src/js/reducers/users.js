832bfbd18b145b550aa7ca268fdcd005852496cf
Delores Diei <delores.diei@andela.com>
2017-05-01 03:09:28 +0100

    add edit user action

--- client/src/js/reducers/users.js
+++ client/src/js/reducers/users.js
@@ -4,6 +4,12 @@ export default (state = {}, action) => {
   switch (action.type) {
     case actionTypes.GET_USER_SUCCESS:
       return { ...state, users: action.users };
+    case actionTypes.USER_UPDATED:
+      const userToUpdate = state.users.find(user => userId === action.userId);
+      const userIndex = state.users.indexOf(userToUpdate);
+      const newUsers = state.users.slice();
+      newUsers[userIndex] = Object.assign({}, newUsers[userIndex], action.user);
+      return { ...state, users: newUsers };
     default:
       return state;
   }
