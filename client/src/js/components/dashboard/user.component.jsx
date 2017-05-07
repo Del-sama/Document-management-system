7745783a77023bf794e606e06fdacb5cf48d7bfa
Delores Diei <delores.diei@andela.com>
2017-05-07 15:24:06 +0100

    feature(component-tests): refactor code [14497853]

--- client/src/js/components/dashboard/user.component.jsx
+++ client/src/js/components/dashboard/user.component.jsx
@@ -11,7 +11,7 @@ import AllDocs from '../docs/allDocs.component';
 import CreateDocument from '../docs/docForm.component';
 import EditDocument from '../../actions/documentManagement/editDocument';
 import DeleteDocument from '../../actions/documentManagement/deleteDocuments';
-
+import Search from '../search.component.jsx';
 
 class UserDashboard extends Component {
   constructor(props) {
@@ -19,7 +19,15 @@ class UserDashboard extends Component {
     this.setEditDocument = this.setEditDocument.bind(this);
     this.setDeleteDocument = this.setDeleteDocument.bind(this);
     this.setViewDocument = this.setViewDocument.bind(this);
-    this.state = {};
+    this.handleSearchBarView = this.handleSearchBarView.bind(this);
+    this.state = {
+      searchBarView: 'noShow'
+    };
+  }
+
+  handleSearchBarView(view) {
+    this.setState({ searchBarView: view });
+    $('ul.tabs').tabs('select_tab', 'searchTab');
   }
 
   setViewDocument(document) {
@@ -78,7 +86,7 @@ class UserDashboard extends Component {
         <div className="mainContainer">
           <div className="bg"></div>
           <Navbar />
-          <Searchbar />
+          <Searchbar handleSearchBarView={this.handleSearchBarView}/>
           <div className="row">
             <div className="tabRow">
               <ul className="tabs tabs-fixed-width">
@@ -88,10 +96,11 @@ class UserDashboard extends Component {
                 <li className="tab "><Link to="#test2">Public Docs</Link></li>
                 <li className="tab "><Link to="#test4">Role Docs</Link></li>
                 <li className="tab "><Link to="#test5">All Docs</Link></li>
+                <li className="tab"><Link to="#searchTab">Search</Link></li>
               </ul>
             </div>
             <div id="test3" className="tabContent col s12">
-              <UserDocs documents={this.props.documents} setEditDocument={this.setEditDocument} setDeleteDocument={this.setDeleteDocument}/>
+              <UserDocs documents={this.props.documents} setViewDocument={this.setViewDocument} setEditDocument={this.setEditDocument} setDeleteDocument={this.setDeleteDocument}/>
             </div>
             <div id="test1" className="tabContent col s12">
               <PrivateDocs documents={this.props.documents} setEditDocument={this.setEditDocument} setDeleteDocument={this.setDeleteDocument} />
@@ -103,8 +112,11 @@ class UserDashboard extends Component {
               <RoleDocs documents={this.props.documents} setEditDocument={this.setEditDocument} setDeleteDocument={this.setDeleteDocument}/>
             </div>
             <div id="test5" className="tabContent col s12">
-            <AllDocs documents={this.props.documents} setViewDocument={this.setViewDocument} />
-          </div>
+              <AllDocs documents={this.props.documents} setViewDocument={this.setViewDocument} />
+            </div>
+            <div id="searchTab" className="tabContent col s12">
+              <Search searchDocuments={this.props.searchDocuments} setViewDocument={this.setViewDocument} searchUsers={this.props.searchUsers} view= {this.state.searchBarView} />
+            </div>
           </div>
           <div></div>
         </div>
