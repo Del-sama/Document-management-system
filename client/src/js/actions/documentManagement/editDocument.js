c393b6dcbd0f4a8825f3a93d604da6affcadcca1
Delores Diei <delores.diei@andela.com>
2017-05-11 13:00:39 +0100

    (feedback): add update document reducer

--- client/src/js/actions/documentManagement/editDocument.js
+++ client/src/js/actions/documentManagement/editDocument.js
@@ -2,19 +2,25 @@ import axios from 'axios';
 import { browserHistory } from 'react-router';
 import * as actionTypes from '../actionTypes';
 
-export default (details, documentid) => {
+export default (details, documentId) => {
   return (dispatch) => {
     const token=window.localStorage.getItem('token');
     if(!token){
-      return
+      return null;
     }
-    return axios.put(`/documents/${documentid}`, details, {
+    details.status = 'waiting';
+    return axios.put(`/documents/${documentId}`, details, {
       headers: {
         Authorization: token
       }
     })
     .then(() => {
-      browserHistory.push('/');
+      dispatch({
+        type: actionTypes.DOCUMENT_UPDATE,
+        status: 'success',
+        documentId
+      })
+      browserHistory.push('/app/dashboard');
     }).catch((err) => {
       dispatch({
         type: actionTypes.DOCUMENT_UPDATE_FAILED,
