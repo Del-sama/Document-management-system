3e200f4868bf6664a5815ea2e8f05d08fae7baf2
Delores Diei <delores.diei@andela.com>
2017-05-12 13:20:37 +0100

    update dom.js

--- client/src/tests/dom.js
+++ client/src/tests/dom.js
@@ -15,3 +15,16 @@ Object.keys(document.defaultView).forEach((property) => {
 global.navigator = {
   userAgent: 'node.js'
 };
+
+// mock calls to localStorage
+global.window.localStorage = {
+  getItem: () => {
+    return `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOjIsIlJvbGVJZCI6MSwidXNlciI6ImFkbWluIiwiaWF0IjoxNDkyODkxNzEzLCJleHAiOjE0OTI5NzgxMTN9.bJ5OMykjtl1xwEwBYK7EJ07qcF95Oi6isIO0PdBwYEQ`;
+  },
+  setItem: () => {
+    // do nothing
+  },
+  clear: () => {
+    // do nothing
+  }
+};
