664151aa6f5e40a8accb23c7d26cd1aac4930201
Delores Diei <delores.diei@andela.com>
2017-05-12 07:16:21 +0100

    comment out code

--- client/src/tests/actions/login.spec.js
+++ client/src/tests/actions/login.spec.js
@@ -1,72 +1,17 @@
-/*
-global expect:true
-global thunk:true
-global configureMockStore:true
-global moxios:true
-global sinon:true
-*/
-import login from '../../js/actions/authorizationManagement/loginAction';
+// import * as actions from '../../js/actions/authorizationManagement/loginAction'
+// import * as actionTypes from '../../js/actions/actionTypes'
 
-const middlewares = [thunk];
-const mockStore = configureMockStore(middlewares);
-
-describe('async actions', () => {
-  beforeEach(() => {
-    moxios.install();
-  });
-
-  afterEach(() => {
-    moxios.uninstall();
-  });
-
-  it.only('creates LOGIN_SUCCESSFUL and LOGIN_ERROR action when user has been validated',
-  (done) => {
-    const expectedActions = [
-      { type: '', payload: { token: 'abc' } },
-      { type: 'LOGIN_ERROR' },
-    ];
-
-    const store = mockStore({ auth: { loggedInUser: null, isAuthenticated: false } });
-
-    store.dispatch(login({
-      userName: 'user',
-      password: 'password'
-    })).then(() => {
-      expect(store.getActions()).to.eql(expectedActions);
-      done();
-    });
-
-    moxios.wait(() => {
-      const request = moxios.requests.mostRecent();
-      request.respondWith({
-        status: 200,
-        response: { token: 'abc' }
-      });
-    });
-  });
-
-  it('creates VALIDATION_ERROR action when user details are incorrect',
-  (done) => {
-    const expectedActions = [
-      { type: 'VALIDATION_ERROR', payload: 'email/passwords do not match' }
-    ];
-
-    const store = mockStore({ auth: { loggedInUser: null, isAuthenticated: false } });
-
-    store.dispatch(login({
-      email: 'wromgemail@gmail.com',
-      password: 'password'
-    })).then(() => {
-      expect(store.getActions()).to.eql(expectedActions);
-      done();
-    });
-
-    moxios.wait(() => {
-      const request = moxios.requests.mostRecent();
-      request.respondWith({
-        status: 401,
-        response: { message: 'email/passwords do not match' }
-      });
-    });
-  });
-});
+// describe('actions', () => {
+//   it('should create an action to login users', () => {
+//     const loginCredentials =
+//     {
+//       userName: 'name',
+//       password: 'password'
+//     }
+//     const expectedAction = {
+//       type: actionTypes.LOGIN_SUCCESSFUL
+//       text
+//     }
+//     expect(actions.addTodo(text)).toEqual(expectedAction)
+//   })
+// })
--- client/src/tests/reducers/documentReducer.spec.js
+++ client/src/tests/reducers/documentReducer.spec.js
@@ -4,6 +4,32 @@ import documentReducer from '../../js/reducers/documents'
 
 
 describe('Document Reducer', () => {
+  const initialDocument = [
+  {
+    id: 9,
+    title: 'Abiku-1',
+    content: 'In vain your bangles cast, charms circle at your feet'
+  },
+  {
+    id: 11,
+    title: 'Abiku-2',
+    content: 'Coming and going these several seasons Do stay'
+  },
+  {
+    id: 13,
+    title: 'class-23',
+    content: 'the best class ever'
+
+  }
+];
+
+const initialState = {
+  documents: initialDocument,
+  documentDetails: false,
+  editMode: false
+};
+let currentState;
+
   it('should return the initial state', () => {
     expect(
       documentReducer(undefined, [])
@@ -11,5 +37,21 @@ describe('Document Reducer', () => {
       []
     )
   });
+
+it(`should load all documents in the store and
+       return the state with all documents`, () => {
+    const documents = [
+      { id: 5,
+        title: 'butterfly',
+        content: 'to pimp a butterfly' },
+      { id: 6, title: 'eyez', content: 'for your eyes only' }
+    ];
+
+    const action = { type: 'DOCUMENT_CREATED', documents };
+    currentState = documentReducer(initialState, action);
+    // expect(currentState.documents).to.have.length(2);
+    expect(currentState.documents.content).toEqual('to pimp a butterfly');
+  });
+
 });
 
